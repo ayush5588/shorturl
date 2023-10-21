ce0cacd616f1ea6a66da38932be8c9191b9f900e
ayush5588 <ayushsinha126@gmail.com>
2023-10-21 17:23:20 +0530

    add return statement

--- cmd/shorturl/main.go
+++ cmd/shorturl/main.go
@@ -26,6 +26,7 @@ func setupRouter() *gin.Engine {
 	router.GET("/healthz", func(c *gin.Context) {
 		logger.Infof("Successfully served GET /healthz request")
 		c.JSON(http.StatusOK, gin.H{"message": "Server is healthy"})
+		return
 	})
 
 	/*
@@ -43,21 +44,25 @@ func setupRouter() *gin.Engine {
 		if err != nil {
 			logger.Errorw("error in unmarshalling the req body", "err", err)
 			c.JSON(http.StatusBadRequest, gin.H{"message": "Please try again."})
+			return
 		}
 
 		if url.OriginalURL == "" {
 			logger.Errorw("invalid req body", "err", ErrURLFieldEmpty)
 			c.JSON(http.StatusBadRequest, gin.H{"message": ErrURLFieldEmpty.Error() + ". Please provide url."})
+			return
 		}
 
-		err = url.URLHandler(c)
+		err = url.URLHandler(c, logger)
 		if err != nil {
 			if errors.Is(err, internal.ErrNotSupportedMethod) {
 				logger.Error(internal.ErrNotSupportedMethod)
 				c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request method."})
+				return
 			}
 			logger.Errorw("internal error", "err", err)
 			c.JSON(http.StatusInternalServerError, gin.H{"message": "Please try again after some time."})
+			return
 		}
 
 	})

c932c2029b897a21c711870596e31d0536760699
ayush5588 <ayushsinha126@gmail.com>
2023-10-21 09:11:51 +0530

    add healthz route definition

--- cmd/shorturl/main.go
+++ cmd/shorturl/main.go
@@ -1,5 +1,26 @@
-package shorturl
+package main
+
+import (
+	"net/http"
+
+	"github.com/ayush5588/shorturl/internal"
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	logger := internal.GetLogger()
+
+	router := gin.Default()
+
+	router.GET("/healthz", func(c *gin.Context) {
+		defer logger.Infof("Successfully served GET /healthz request")
+		c.JSON(http.StatusOK, gin.H{"message": "Server is healthy"})
+	})
+
+	return router
+}
 
 func main() {
-
+	r := setupRouter()
+	r.Run(":8080")
 }

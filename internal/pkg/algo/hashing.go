82c8f7efd3a2d09e3b1450d13d3ce33d774e42b9
ayush5588 <ayushsinha126@gmail.com>
2023-10-22 02:36:33 +0530

    add implementation and dummy hashing function

--- internal/pkg/algo/hashing.go
+++ internal/pkg/algo/hashing.go
@@ -1,1 +1,6 @@
 package algo
+
+// Hashing ...
+func Hashing(val string) (string, error) {
+	return "abc", nil
+}
--- internal/short.go
+++ internal/short.go
@@ -1,8 +1,11 @@
 package internal
 
 import (
+	"encoding/json"
 	"errors"
 
+	"github.com/ayush5588/shorturl/db"
+	"github.com/ayush5588/shorturl/internal/pkg/algo"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
 	"go.uber.org/zap"
@@ -21,17 +24,31 @@ type URL struct {
 	Database
 }
 
+// URLInfo ...
+type URLInfo struct {
+	OriginalURL string `json:"originalURL" redis:"originalURL"`
+	Alias       string `json:"alias" redis:"alias"`
+}
+
 var (
 	// ErrNotSupportedMethod ...
 	ErrNotSupportedMethod = errors.New("not supported method")
 )
 
 var (
-	domain = "http://localhost:8080/"
+	domain         = "http://localhost:8080/"
+	origToShortKey = "original:to:short"
+	shortToOrigKey = "short:to:original"
 )
 
 // URLHandler ...
 func (u *URL) URLHandler(c *gin.Context, logger *zap.SugaredLogger) error {
+	redisClient, err := db.NewRedisConnection()
+	if err != nil {
+		return err
+	}
+	u.client = redisClient
+
 	switch c.Request.Method {
 	case "GET", "":
 		// Handle URL redirect request
@@ -40,9 +57,62 @@ func (u *URL) URLHandler(c *gin.Context, logger *zap.SugaredLogger) error {
 	case "PUT":
 		// Handle ShortURL generate request
 		logger.Info("Inside PUT of URLHandler", u.OriginalURL)
-
-		return nil
+		return u.shortenURLHandler(logger)
 	default:
 		return ErrNotSupportedMethod
 	}
+}
+
+func (u *URL) shortenURLHandler(logger *zap.SugaredLogger) error {
+	origURL := u.OriginalURL
+
+	// Check in db if there exist an entry for the given originalURL
+	val, err := u.client.HGet(origToShortKey, origURL).Result()
+	if err != nil && err != redis.Nil {
+		logger.Errorf("error in getting the value from db for %s key & %s field", origToShortKey, origURL)
+		return err
+	}
+
+	if val != "" {
+		logger.Infof("value exist for originalURL: %s", origURL)
+		u.ShortURL = domain + val
+		return nil
+	}
+
+	// This is a first time shortening request for the url
+	uid, err := algo.Hashing(origURL)
+	if err != nil {
+		logger.Errorf("error in getting uid from hashing of originalURL: %s", origURL)
+		return err
+	}
+
+	urlMap := URLInfo{
+		OriginalURL: origURL,
+		Alias:       u.Alias,
+	}
+
+	urlMapbytes, err := json.Marshal(urlMap)
+	if err != nil {
+		logger.Errorf("error in marshaling of urlMap: %+v", urlMap)
+		return err
+	}
+
+	_, err = u.client.HSet(shortToOrigKey, uid, urlMapbytes).Result()
+	if err != nil {
+		logger.Errorf("error in entering urlMap: %+v for uid: %s", urlMap, uid)
+		return err
+	}
+
+	_, err = u.client.HSet(origToShortKey, origURL, uid).Result()
+	if err != nil {
+		logger.Errorf("error in entering mapping between originalURL: %s & uid: %s", origURL, uid)
+		return err
+	}
+
+	u.ShortURL = domain + uid
+
+	logger.Info("Success URLHandler operation")
+
+	return nil
+
 }
